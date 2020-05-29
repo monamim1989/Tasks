dbdf5a0e52ac3810fc597580eeb14ef75f64dec9
monamim1989 <mmukhopa@stevens.edu>
2020-05-29 13:52:42 -0400

    task updates

---
+++ Task4/jquery-ajax-github/WebContent/js/script.js
@@ -0,0 +1,49 @@
+$.ajax({
+	url: 'https://api.github.com/users',
+	type: "get",
+	dataType: "json",
+	   
+	success: function(data) {
+	    generateTable(data);
+	}
+});
+	
+function generateTable(data) {
+	for (var i = 0; i < data.length; i++) {
+	    addRow(data[i]);
+	}
+}
+	
+function addRow(rowData) {
+	var row = $("<tr />")
+	$("#githubUserDetails").append(row); 
+	row.append($("<td>" + rowData.login + "</td>"));
+	row.append($("<td>" + "<img src='" + rowData.avatar_url + "'/></td>"));
+	row.append($("<td>" + rowData.site_admin + "</td>"));
+	
+	$.ajax({
+	  type: "get",
+	  url: 'https://api.github.com/users/'+rowData.login,
+	  dataType: "json",
+	        
+	  success: function(data) {
+	     var buttonId = "btn" + rowData.login;
+	     row.append($("<td>" + `<button class='btn btn-success' id=${buttonId}>?</button>` + "</td>"));
+	 	 
+	     $(`#${buttonId}`).click(function() {
+	 		console.log(`btn clicked! : ${buttonId}`)	 		
+	 		$.ajax({
+	 			type: "get",
+	 			url: "https://api.github.com/users/"+rowData.login,
+	 			dataType: "json", 			
+	 		}).then(function(data) {
+	 			$(`#${buttonId}`).html(data.followers);
+	 		})
+	 	 })
+	 	 
+	 	row.append($("<td>" + data.following + "</td>"));
+	    row.append($("<td>" + data.public_repos + "</td>"));
+	  }
+	  
+	});
+}
